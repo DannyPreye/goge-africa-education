abf63f0d4d434ce9be055a934963fe3c9c45ccf7
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-08-13 21:02:12 +0100

    Made changes to the pages

--- components/coursePage/CoursesSection.jsx
+++ components/coursePage/CoursesSection.jsx
@@ -2,6 +2,8 @@ import { BiSearch } from "react-icons/bi";
 import CourseCard from "../Home/CourseCard";
 import { motion } from "framer-motion";
 import { courses } from "../../test/test";
+import { AiFillAppstore } from "react-icons/ai";
+
 const SearchComponent = () => (
   <motion.div
     whileInView={{ y: 0 }}
@@ -29,17 +31,31 @@ const SearchComponent = () => (
 );
 
 export default () => (
-  <section className="w-screen bg-white">
+  <section className="w-screen bg-white ">
     <div className="container mx-auto grid place-items-center">
       <SearchComponent />
     </div>
-    <div className="w-[90%] flex gap-[67px]">
+    <div className="w-[90%]  gap-[67px] grid place-items-center">
       <div>
+        <div className="mb-[47px]">
+          <div className="w-[328px] flex justify-between ">
+            <p>Showing 1–8 of 10 results</p>
+            <div className="flex w-[120px] justify-between">
+              <p>Show</p>
+              <p>6</p>
+              <p>12</p>
+              <p>16</p>
+            </div>
+          </div>
+        </div>
+
         {/* Course Card */}
-        <div className="grid grid-cols-3">
-          {courses.map((course, index) => (
-            <CourseCard topic={course.topic} type={course.type} />
-          ))}
+        <div className="grid grid-cols-3 w-[928px] gap-x-[9px] gap-y-[15px]">
+          {courses.map((course, i) => {
+            return (
+              <CourseCard key={i} type={course.type} topic={course.topic} />
+            );
+          })}
         </div>
       </div>
     </div>
